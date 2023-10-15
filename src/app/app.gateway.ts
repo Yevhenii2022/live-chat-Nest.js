db1ba4b66a692d9423989e22d4ff3962199d0dbc
Yevhenii2022 <3starosta3@gmail.com>
2023-10-15 10:15:00 +0300

    finished project

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,36 +1,41 @@
 import {
-	OnGatewayConnection,
-	OnGatewayDisconnect,
-	OnGatewayInit,
-	SubscribeMessage,
-	WebSocketGateway,
-	WebSocketServer,
-} from "@nestjs/websockets";
-import { Prisma } from "@prisma/client";
-import { Server, Socket } from "socket.io";
-import { AppService } from "src/app.service";
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Prisma } from '@prisma/client';
+import { Server, Socket } from 'socket.io';
+import { AppService } from 'src/app.service';
 
 @WebSocketGateway()
-export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-	constructor(private appService: AppService) {}
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private appService: AppService) {}
 
-	@WebSocketServer() server: Server;
+  @WebSocketServer() server: Server;
 
-	@SubscribeMessage("sendMessage")
-	async handleSendMessage(client: Socket, payload: Prisma.ChatCreateInput): Promise<void> {
-		await this.appService.createMessage(payload);
-		this.server.emit("recMessage", payload);
-	}
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(
+    client: Socket,
+    payload: Prisma.ChatCreateInput,
+  ): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
 
-	afterInit(server: any) {
-		console.log(server);
-	}
+  afterInit(server: any) {
+    console.log(server);
+  }
 
-	handleConnection(client: Socket) {
-		console.log(`Connected: ${client.id}`);
-	}
+  handleConnection(client: Socket) {
+    console.log(`Connected: ${client.id}`);
+  }
 
-	handleDisconnect(client: Socket) {
-		console.log(`Disconnected: ${client.id}`);
-	}
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { PrismaService } from "./prisma.service";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-	const prismaService = app.get(PrismaService);
-	await prismaService.enableShutdownHooks(app);
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
 
-	await app.listen(3000);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
